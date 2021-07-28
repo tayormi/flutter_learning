d369a3b985f4ace1d4dbd03025fde7c765a33f12
Temitope Ajiboye <tayormi@gmail.com>
2021-07-29 00:14:55 +0100

    Handle Navigations in Flutter

---
+++ lib/navigation/second_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go back'),
+        ),
+      ),
+    );
+  }
+}
